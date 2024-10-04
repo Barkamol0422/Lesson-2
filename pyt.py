08415bc21ecf545d0dbea05357c200669957fd59
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2024-10-04 21:33:03 +0500

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,6 @@
+a=input('enter your birth month: ')
+b=input('enter your birth month: ')
+c=input('enter your birth month: ')
+d=input('enter your birth month: ')
+e=input('enter your birth month: ')
+print(a,b,c,d,e)
