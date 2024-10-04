750934b82040a3f74dbe5b87d4bdfdc45d683f50
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2024-10-04 21:18:58 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,3 @@
+a=input('enter your name:')
+b=input('enter your birth month:')
+print(a,'is born on',b)
---
+++ pyton.py
@@ -0,0 +1,3 @@
+a=int(input('Write first number: '))
+b=int(input('Write second number: '))
+print(a+b)
